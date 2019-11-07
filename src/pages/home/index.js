38a68e85d40ac95ea168d11209e42a1757b35696
LinearLaw <17817363770@163.com>
2019-11-07 23:37:55 +0800

    新增首页三个轮播图

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, WingBlank, WhiteSpace, Carousel, Tabs } from 'antd-mobile';
+import { Flex, WingBlank, WhiteSpace, Carousel, Tabs ,Grid } from 'antd-mobile';
 import "./index.css"
 
 class Home extends React.Component {
@@ -7,39 +7,101 @@ class Home extends React.Component {
         super(props);
         this.state = {
             searchValue: [
-                { title: "通勤出街显美小包" },
-                { title: "DW粉底液1W1" },
-                { title: "秋冬男士气质穿搭" },
-                { title: "气质连衣裙" },
+                { 
+                    title: "通勤出街显美小包",
+                    img:"http://img.alicdn.com/tfs/TB1RZVRkYr1gK0jSZR0XXbP8XXa-1500-530.jpg_760x760Q90s50.jpg_.webp"    
+                },
+                { 
+                    title: "DW粉底液1W1" ,
+                    img:"//img.alicdn.com/tfs/TB1MtMnlQP2gK0jSZPxXXacQpXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
+                },
+                { 
+                    title: "秋冬男士气质穿搭" ,
+                    img: "//img.alicdn.com/tfs/TB1WxT2loH1gK0jSZSyXXXtlpXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
+                },
+                { 
+                    title: "气质连衣裙" ,
+                    img:"//img.alicdn.com/tfs/TB13JTBlkY2gK0jSZFgXXc5OFXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
+                }
             ],
             tabs: [
-                { title: '首页' },
-                { title: '美妆' },
-                { title: '百货' },
-                { title: '女装' },
-                { title: '食品' },
-                { title: '生鲜' },
-                { title: '家装' },
-                { title: '母婴' },
-                { title: '保健' },
-                { title: '鞋靴' },
-                { title: '手机' },
-                { title: '箱包' },
-                { title: '男装' },
-                { title: '饰品' },
-                { title: '洗护' },
-                { title: '电器' },
+                { 
+                    text: '首页' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/20983/16/10753/6124/5c8a16aaE5d6b15d7/01e0e818a7505267.png"
+                },
+                {
+                    text: '美妆' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/39401/17/2391/5859/5cc06fcfE2ad40668/28cda0a571b4a576.png"
+                },
+                { 
+                    text: '百货' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/17169/3/4127/4611/5c2f35cfEd87b0dd5/65c0cdc1362635fc.png"
+                },
+                { 
+                    text: '女装' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t17725/156/1767366877/17404/f45d418b/5ad87bf0N66c5db7c.png"
+                },
+                { 
+                    text: '食品' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t16990/157/2001547525/17770/a7b93378/5ae01befN2494769f.png"
+                },
+                { 
+                    text: '生鲜' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t18454/342/2607665324/6406/273daced/5b03b74eN3541598d.png"
+                },
+                { 
+                    text: '家装' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t22228/270/207441984/11564/88140ab7/5b03fae3N67f78fe3.png"
+                },
+                { 
+                    text: '母婴' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/7068/29/8987/5605/5c120da2Ecae1cc3a/016033c7ef3e0c6c.png"
+                },
+                { 
+                    text: '保健' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t16828/63/2653634926/5662/d18f6fa1/5b03b779N5c0b196f.png"
+                },
+                { 
+                    text: '鞋靴' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/22262/9/1470/4527/5c120cd0E5d3c6c66/4792ec307a853e9f.png"
+                },
+                { 
+                    text: '手机' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/14555/23/1446/4195/5c120e71E947f3ca1/cdbad904f61e1fb7.png"
+                },
+                { 
+                    text: '箱包' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/19745/21/1415/4886/5c120cbbEe58af0bf/4d19bf58d42fc9c4.png"
+                },
+                { 
+                    text: '男装' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/25651/12/1391/4836/5c120c34Eb93a70fa/3a1735f6a79d0721.png"
+                },
+                { 
+                    text: '饰品' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/14996/29/2195/11094/5c1afdb7Ee54cf069/d6ab6a0f64fee068.png"
+                },
+                { 
+                    text: '洗护' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/27748/22/1419/4913/5c120d0eEa52ea62f/3c2b78a40edc97b2.png"
+                },
+                { 
+                    text: '电器' ,
+                    icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/19699/10/1440/6755/5c120ca6Eaa521f62/f9d57f39e55c7321.png"
+                },
             ],
-            carousels:['1', '2', '3'],
             imgHeight: 176
         }
     }
     render() {
+        const data1 = Array.from(new Array(this.state.tabs.length)).map((v,i) => ({
+            title:this.state.tabs[i].text ,
+        }));
         return (
             <div className="homePage">
                 <WhiteSpace />
+                {/* 头部 */}
                 <WingBlank size="md">
-                    {/* 头部 */}
                     <Flex justify="between">
                         <img src="assets/logo.png" style={{ width: '35px' }} alt="" />
                         <Flex justify="between" align="center" className="searchBar">
@@ -49,7 +111,7 @@ class Home extends React.Component {
                                 dots={false}
                                 dragging={false}
                                 swiping={false}
-                                autoplay
+                                autoplay = {true}
                                 infinite
                             >
                                 {
@@ -66,7 +128,7 @@ class Home extends React.Component {
                     </Flex>
                 </WingBlank>
                 {/* Tabs选择 */}
-                <Tabs tabs={this.state.tabs} renderTabBar={props => <Tabs.DefaultTabBar {...props} page={6} />}>
+                <Tabs tabs={data1} renderTabBar={props => <Tabs.DefaultTabBar {...props} page={6} />}>
                     {this.renderContent}
                 </Tabs>
                 {/* 轮播图 */}
@@ -74,18 +136,17 @@ class Home extends React.Component {
                     autoplay={true}
                     infinite
                 >
-                    {this.state.carousels.map(val => (
+                    {this.state.searchValue.map(val => (
                         <a
                             key={val}
                             href="http://www.alipay.com"
                             style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
                         >
                             <img
-                                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                src={val.img}
                                 alt=""
                                 style={{ width: '100%', verticalAlign: 'top' }}
                                 onLoad={() => {
-                                    // fire window resize event to change height
                                     window.dispatchEvent(new Event('resize'));
                                     this.setState({ imgHeight: 'auto' });
                                 }}
@@ -93,6 +154,10 @@ class Home extends React.Component {
                         </a>
                     ))}
                 </Carousel>
+                {/* 宫格 */}
+                <WingBlank size="md">
+                    <Grid data={this.state.tabs} isCarousel columnNum = "5" dotStyle={dotStyles} hasLine="false"/>
+                </WingBlank>
                 <div></div>
 
             </div>
@@ -100,3 +165,6 @@ class Home extends React.Component {
     }
 }
 export default Home;
+const dotStyles={
+
+}
