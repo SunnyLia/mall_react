8b987020c848dda0a3d637f40b51df23fde585a9
sunnyLia <1326155622@qq.com>
2019-11-08 16:26:38 +0800

    首页新增模板

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{Fragment} from 'react';
 import { Flex, WingBlank, WhiteSpace, Carousel, Tabs ,Grid } from 'antd-mobile';
 import "./index.css"
 
@@ -14,6 +14,14 @@ class Home extends React.Component {
                 { 
                     title: "DW粉底液1W1" ,
                     img:"//img.alicdn.com/tfs/TB1MtMnlQP2gK0jSZPxXXacQpXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
+                },
+                { 
+                    title: "秋冬男士气质穿搭" ,
+                    img: "//img.alicdn.com/tfs/TB1WxT2loH1gK0jSZSyXXXtlpXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
+                },
+                { 
+                    title: "气质连衣裙" ,
+                    img:"//img.alicdn.com/tfs/TB13JTBlkY2gK0jSZFgXXc5OFXa-1500-530.jpg_760x760Q90s50.jpg_.webp"
                 },
                 { 
                     title: "秋冬男士气质穿搭" ,
@@ -90,15 +98,125 @@ class Home extends React.Component {
                     icon:"//m.360buyimg.com/mobilecms/s120x120_jfs/t1/19699/10/1440/6755/5c120ca6Eaa521f62/f9d57f39e55c7321.png"
                 },
             ],
-            imgHeight: 176
+            imgHeight: 176,
+            lists:[
+                {
+                    title:"每日秒杀",
+                    prods:[
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/n1/s150x150_jfs/t1/65000/23/14266/146571/5dbc0854E3c952de7/cce8829ec944181a.jpg!q70.jpg.dpg'
+                        },
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/n1/s150x150_jfs/t17677/311/1731306712/38153/32cc1d8d/5ad40f09Ncd7d80d6.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/n1/s150x150_jfs/t29566/227/1464891645/10350/a5b133e2/5ce20cdcNd9cdd972.jpg!q70.jpg.dpg'
+                        },
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/56939/39/1210/59272/5cefa52dE2f70588a/3174a20e743edd49.png!q70.jpg.dpg'
+                        }
+                    ]
+                },
+                {
+                    title:"精选会场",
+                    prods:[
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/n1/s150x150_jfs/t29566/227/1464891645/10350/a5b133e2/5ce20cdcNd9cdd972.jpg!q70.jpg.dpg'
+                        },
+                        {
+                            text:'平板电脑',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/56939/39/1210/59272/5cefa52dE2f70588a/3174a20e743edd49.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/38988/36/7707/54292/5cefa2b0E1cce5cfa/64e76ed6c67568bf.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/71612/30/758/62076/5cefa5a6E2494c8c2/13aac3e0bb1b4e55.png!q70.jpg.dpg'
+                        }
+                    ]
+                },
+                {
+                    title:"精选会场",
+                    prods:[
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/50174/30/1230/9784/5cef98bfE318f8d24/4720dc4079dd568b.jpg!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/34938/26/11155/70774/5cefa0c6E5a90edb2/47b686712510d44e.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/38988/36/7707/54292/5cefa2b0E1cce5cfa/64e76ed6c67568bf.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/71612/30/758/62076/5cefa5a6E2494c8c2/13aac3e0bb1b4e55.png!q70.jpg.dpg'
+                        }
+                    ]
+                },
+                {
+                    title:"精选会场",
+                    prods:[
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/38988/36/7707/54292/5cefa2b0E1cce5cfa/64e76ed6c67568bf.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/71612/30/758/62076/5cefa5a6E2494c8c2/13aac3e0bb1b4e55.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/38988/36/7707/54292/5cefa2b0E1cce5cfa/64e76ed6c67568bf.png!q70.jpg.dpg'
+                        },
+                        {
+                            text:'美妆爆品',
+                            img:'//m.360buyimg.com/mobilecms/s150x150_jfs/t1/71612/30/758/62076/5cefa5a6E2494c8c2/13aac3e0bb1b4e55.png!q70.jpg.dpg'
+                        }
+                    ]
+                }
+            ]
         }
     }
+    componentDidMount() {
+        // var to = new Date('2019, 11, 12, 00, 00, 00'); //月份是从0开始的
+        var to = new Date('2019/11/12 00:00:00');
+ 
+        function antitime() {
+            var now = new Date();
+            var deltaTime = to - now; 
+            if (deltaTime <= 0) {
+                clearInterval(this.timer);
+                return;
+            }
+            
+            var h = Math.floor(deltaTime / 3600000); 
+            var m = Math.floor(deltaTime / 60000 % 60); 
+            var s = Math.floor(deltaTime / 1000 % 60);
+                
+            var timeStr = "" + (h<10?"0"+h:h)+":" + (m<10?"0"+m:m)+":" + (s<10?"0"+s:s);
+            document.getElementsByClassName("secKill")[0].innerHTML=timeStr
+        }
+        this.timer = setInterval(antitime, 1000);
+    }
+    componentWillUnmount() {
+        this.timer && clearInterval(this.timer);
+      }
     render() {
         const data1 = Array.from(new Array(this.state.tabs.length)).map((v,i) => ({
             title:this.state.tabs[i].text ,
         }));
         return (
-            <div className="homePage">
+            <div className="homePage" style={{marginBottom:'50px'}}>
                 <WhiteSpace />
                 {/* 头部 */}
                 <WingBlank size="md">
@@ -158,8 +276,76 @@ class Home extends React.Component {
                 <WingBlank size="md">
                     <Grid data={this.state.tabs} isCarousel columnNum = "5" dotStyle={dotStyles} hasLine="false"/>
                 </WingBlank>
-                <div></div>
-
+                <WhiteSpace size="lg"/>
+                <div style={{paddingTop: '12px',overflow: 'hidden'}}>
+                    <Carousel className="space-carousel"
+                    frameOverflow="visible"
+                    cellSpacing={10}
+                    slideWidth={0.6}
+                    autoplay={false}
+                    dots={false}
+                    infinite
+                    afterChange={index => this.setState({ slideIndex: index })}
+                    >
+                    {(this.state.searchValue.slice(0,Math.floor(this.state.searchValue.length/2))).map((value,index) => (
+                        <Flex key={index}
+                        style={{
+                            position: 'relative',
+                            top: this.state.slideIndex === index ? -10 : 0,
+                            height: this.state.imgHeight
+                        }}>
+                            <Flex.Item>
+                                <img
+                                    src={this.state.searchValue[2*index].img}
+                                    alt=""
+                                    style={{ width: '100%', verticalAlign: 'top' }}
+                                    onLoad={() => {
+                                        window.dispatchEvent(new Event('resize'));
+                                        this.setState({ imgHeight: 'auto' });
+                                    }}
+                                />
+                            </Flex.Item>
+                            <Flex.Item>
+                                <img
+                                    src={this.state.searchValue[2*index+1].img}
+                                    alt=""
+                                    style={{ width: '100%', verticalAlign: 'top' }}
+                                    onLoad={() => {
+                                        window.dispatchEvent(new Event('resize'));
+                                        this.setState({ imgHeight: 'auto' });
+                                    }}
+                                />
+                            </Flex.Item>
+                        </Flex>
+                    ))}
+                    </Carousel>
+                </div>
+                <WhiteSpace size="lg"/>
+                <WingBlank size="md">
+                <Flex wrap="wrap">
+                    {
+                        this.state.lists.map((val,i)=>(
+                            <div style={{width:'50%'}} key={i}>
+                                <div style={{fontWeight:'bold',padding: '8px 7px 4px'}}>{val.title}
+                                {
+                                    i==0 && (
+                                        <span className="secKill">00:00:00</span>
+                                    )
+                                }
+                                </div>
+                                <Flex>
+                                    <div style={{width:'50%',padding: '5px'}}>
+                                        <img src={val.prods[0].img} alt="" style={{width:'100%'}} />
+                                    </div>
+                                    <div style={{width:'50%',padding: '5px'}}>
+                                        <img src={val.prods[1].img} alt="" style={{width:'100%'}} />    
+                                    </div>
+                                </Flex>
+                            </div> 
+                        ))
+                    }
+                </Flex>
+                </WingBlank>
             </div>
         )
     }
